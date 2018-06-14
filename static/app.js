a5f1b55fa598f2fb7910b6ba742cd9080e970ff3
rennyvick <irenevky85@gmail.com>
2018-06-14 10:10:55 +0300

    first commit

---
+++ static/app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+ 'my first todo item',
+ 'my second todo item',
+ 'my third todo item'
+    ],
+    item: 'To do item'
+  },
+  methods: {
+  	addTodo(){
+  		this.todos.push(this.item)
+  		this.item='';
+  	}
+  }
+})
